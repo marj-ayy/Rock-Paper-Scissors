419a71a183c4ff709356fb3284d65359eec74ca2
Marouane <184505120+marj-ayy@users.noreply.github.com>
2024-11-21 13:03:26 +0100

    Add one round player function

--- rps.js
+++ rps.js
@@ -25,4 +25,54 @@ function getHumanChoice () {
     return choice;
 }
 
-getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound (humanChoice, computerChoice) {
+
+    let combination = humanChoice + computerChoice;
+    let message;
+    let win = "You win ! ";
+    let lose = "You lose ! ";
+    let paper = "Paper beats Rock.";
+    let rock = "Rock beats Scissors.";
+    let scissors = "Scissors beat Paper.";
+
+    if (humanChoice === computerChoice){
+        message = "It's a draw !";
+    }
+    switch (combination) {
+        case "rockpaper":
+            message = lose + paper;
+            ++computerScore;
+            break;
+        case "rockscissors":
+            message = win + rock;
+            ++humanScore;
+            break;
+        case "paperrock":
+            message = win + paper;
+            ++humanScore;
+            break;
+        case "paperscissors":
+            message = lose + scissors;
+            ++computerScore;
+            break;
+        case "scissorsrock":
+            message = lose + rock;
+            ++computerScore;
+            break;
+        case "scissorspaper":
+            message = win + scissors;
+            ++humanScore;
+            break;
+    }
+    console.log("The computer chose : " + computerChoice);
+    console.log(message);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log("Human score : " + humanScore + " and computer score : " + computerScore + ".");
